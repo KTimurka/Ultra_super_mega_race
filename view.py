8e1d526df4f325b357c082f1ae65488492304782
KTimurka <kurbanov.tm@phystech.edu>
2021-11-26 10:37:53 +0300

    Ура, запустилось хоть что то

--- view.py
+++ view.py
@@ -1,6 +1,8 @@
+import pygame
+
 def draw_car(screen,actions,obj,scaled_image):
     x= obj.x
     y= obj.y
     alpha = obj.angle
-    rotated_image = pygame.transform.rotate(scaled_image, angle)
+    rotated_image = pygame.transform.rotate(scaled_image, alpha)
     screen.blit(rotated_image, (x, y))
